385d36a8a789b7db22e855801f7b9f8d5eb0685f
PArthem <pesnya.arthem@gmail.com>
2025-05-04 00:05:16 +0300

    feat(less1@0.1.0): реализована простая интерактиваная игра, на одного пользователя, если пользователь найдет одно из трех мест, ему выведется сообщение в чате

---
+++ less1/test.py
@@ -0,0 +1,38 @@
+import sys
+import os
+from collections import namedtuple
+
+from pathlib import Path
+from time import sleep
+
+sys.path.append("C:\\Users\\pesny\\projects\\learning\\game-dev\\minecraft\\Minecraft_Tools_1_19_Python_3_10\\Minecraft_Tools_1_19_Python_3_10\\minecraftPythonAPI\\py3minepi-master")
+
+import mcpi.minecraft as minecraft
+craft = minecraft.Minecraft.create(address="minecraft-server.hcloud")
+Cor = namedtuple("Cor",["x","y","z"])
+game_location = Cor(x=-15,y=13,z=-16)
+game_location2 = Cor(x=-14,y=0,z=38)
+game_location3 = Cor(x=25,y=-1,z=38)
+game_location4 = Cor(x=6,y=-15,z=38)
+find_once_loc = Cor(x=None,y=None,z=None)
+print(game_location)
+# x(СеверЮг):6    y(НизВерх):-15, z(ЗападВосток):38
+while True:
+    cor = craft.player.getTilePos()
+    print(f"x(СеверЮг):{cor.x}\ty(НизВерх):{cor.y},\tz(ЗападВосток):{cor.z}")
+    
+    if (cor == game_location ): 
+        if (find_once_loc != cor):
+            craft.postToChat("Ура,ты нашёл место Полины")
+    elif (cor == game_location2):
+        if (find_once_loc != cor):
+            craft.postToChat("Ура,ты нашёл место Ильи")
+    elif (cor == game_location3):
+        if (find_once_loc != cor):  
+            craft.postToChat("Ура,ты нашёл место Артёма")
+    elif (cor == game_location4):
+        if (find_once_loc != cor):
+            craft.postToChat("Ура ты нашел место Папы!")
+    
+    find_once_loc = cor
+    sleep(0.5)
